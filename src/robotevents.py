b3598242d5a2ec9f21fd11d6138046e4177c14e9
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-27 11:34:13 +0800

    some error handling ..

--- src/robotevents.py
+++ src/robotevents.py
@@ -1,7 +1,8 @@
+import logging
 from typing import Any
 import requests
 from enum import Enum
-
+import db
 from tables import Qualification, Teams
 
 class RobotEvents:
@@ -9,6 +10,7 @@ class RobotEvents:
     header: dict[str, str]
     base: str
     season: int
+    logger: logging.Logger = logging.getLogger(__name__)
 
     def __init__(self, token: str):
         self.token = token
@@ -18,20 +20,24 @@ class RobotEvents:
             "Authorization": f"Bearer {token}"
         }
 
-    def request(self, path: str) -> Any:  # pyright: ignore[reportExplicitAny, reportAny]
+    def request(self, path: str) -> Any | None:  # pyright: ignore[reportExplicitAny, reportAny]
         if path[0] != "/":
-            print("path needs to start with a blackslash")
+            self.logger.exception("path needs to start with a blackslash")
             return (None)
         url = self.base + path
         res = requests.get(url, headers=self.header)
         try:
             res.raise_for_status()
-        except requests.HTTPError as e:
-            print(e)
+        except requests.RequestException as exc:
+            self.logger.exception("API request failed: %s %s", url, exc)
+            return None
         return res.json()  # pyright: ignore[reportAny]
 
     def get_qualifications(self, robotevents_id: int) -> Qualification:
-        awards = self.request(f"/teams/{str(robotevents_id)}/awards?season%5B%5D={self.season}")["data"]
+        awards = self.request(f"/teams/{str(robotevents_id)}/awards?season%5B%5D={self.season}")
+        if not awards:
+            return Qualification.NONE
+        awards = awards["data"]
         highest = Qualification.NONE
         for award in awards:
             # print(award)
@@ -43,24 +49,21 @@ class RobotEvents:
         return highest
 
 
-    def create_team(self, robotevents_id: int, score: tuple[int, int, int]) -> Teams:
-        url = self.base + f"/teams/{robotevents_id}"
-        res = requests.get(url, headers=self.header)
-        try:
-            res.raise_for_status()
-        except requests.HTTPError as e:
-            print(e)
-        res = res.json()  # pyright: ignore[reportAny]
-        # team = Teams(
-        #     id=res['id'],  # pyright: ignore[reportAny] 
-        #     number=res['number'],  # pyright: ignore[reportAny]
-        #     organization = res['organization'],   # pyright: ignore[reportAny]
-        #     country= res['location']['country'],  # pyright: ignore[reportAny]
-        #     registered = res['registered'],  # pyright: ignore[reportAny]
-        #     grade = res['grade'],  # pyright: ignore[reportAny]
-        #     score = score[0],
-        #     programming = score[1],
-        #     driver = score[2],
-        #
-        # )
-        # return team
+    def create_team(self, robotevents_id: int, score: tuple[int, int, int]):
+        res = self.request(f"/teams/{robotevents_id}")
+        if not res:
+            return None
+        team = Teams(
+            id=res['id'],  # pyright: ignore[reportAny] 
+            number=res['number'],  # pyright: ignore[reportAny]
+            organization = res['organization'],   # pyright: ignore[reportAny]
+            country= res['location']['country'],  # pyright: ignore[reportAny]
+            registered = res['registered'],  # pyright: ignore[reportAny]
+            grade = res['grade'],  # pyright: ignore[reportAny]
+            qualification =  self.get_qualifications(res['id']),  # pyright: ignore[reportAny]
+            score = score[0],
+            programming = score[1],
+            driver = score[2],
+        )
+        return team
+
