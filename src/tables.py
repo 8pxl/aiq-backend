d118fdc65e42af12609261bd69c66bbd60a1b9c3
8pxl <56054380+8pxl@users.noreply.github.com>
2025-11-27 11:08:36 +0800

    add qualification enum

---
+++ src/tables.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from sqlmodel import Field, SQLModel
+from enum import Enum
+
+class Qualification(Enum):
+    NONE = 0
+    REGIONAL = 1
+    WORLD = 2
+
+    @classmethod
+    def from_string(cls, s: str) -> Qualification:
+        mapping = {
+            "Event Region Championship": cls.REGIONAL,
+            "World Championship": cls.WORLD
+        }
+        return mapping.get(s, cls.NONE)
+
+class Teams(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    number: str
+    organization: str
+    country: str
+    registered: bool
+    grade: str
+    qualification: Qualification
+    score: int
+    programming: int
+    driver: int
